68a142bccec144842829f05b0a6b91d624b78a01
Marcelo Nascimento <marcelo.wendergyn@gmail.com>
2020-11-06 00:41:06 -0300

    Implementada a integração com Bing Search News

--- project-b-webapi-bing/Controllers/PesquisaController.cs
+++ project-b-webapi-bing/Controllers/PesquisaController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.CognitiveServices.Search.CustomSearch;
 using Microsoft.Azure.CognitiveServices.Search.CustomSearch.Models;
 using project_b_webapi.Models;
+using project_b_webapi_bing.Models;
+
 namespace project_b_webapi.Controllers
 {
     [ApiController]
@@ -17,48 +19,27 @@ namespace project_b_webapi.Controllers
         [HttpGet]
         public IList<string> Get()
         {
-            var result = new List<string> {"Marcelo", "Marcos", "Celma", "Valquiria"};
+            var result = new List<string> { "Marcelo", "Marcos", "Celma", "Valquiria" };
 
             return result.ToArray();
         }
 
         [HttpGet("{id}")]
-        public async Task<DtoResultadoPesquisa> GetAsync(int id)
+        public async Task<BingNewsResponse> Get(int id)
         {
-            string subscriptionKey = "04320c3a-437f-4a75-af5f-e5e2b6aaf002";
-            var credential = new ApiKeyServiceClientCredentials(subscriptionKey);
-            var client = new CustomSearchClient(credential);
-            var webData = await client.CustomInstance.SearchAsync("0", "Bolsonaro");
-            if (webData?.WebPages?.Value?.Count > 0)
-            {
-                var firstWebPagesResult = webData.WebPages.Value.FirstOrDefault();
+            var client = new BingSearchClient();
+            var response = await client.Request("Bolsonaro");
 
-                if (firstWebPagesResult != null)
-                {
-                    Console.WriteLine(firstWebPagesResult.WebSearchUrl);
-                }
-                else
-                {
-                    Console.WriteLine("Couldn't find web results!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Didn't see any Web data..");
-            }
+            return response;
+        }
 
-            var result = new DtoResultadoPesquisa
-            {
-                Id = 145714,
-                Candidato = new DtoCandidato { Id = 88, Nome = "LULULUL", Partido = "PT", UF = "RJ" },
-                Noticias = new List<DtoNoticia> () 
-                {
-                    new DtoNoticia { Corpo = "Lula corrupto bla bla bla", Titulo = "Lula condenado bla bla bla", Imagem = "https://media.gazetadopovo.com.br/2018/04/db4331c5568be0a7a0a82602ff2f25a1-gpMedium.jpg"},
-                    new DtoNoticia { Corpo = "Bolsonaro corrupto bla bla bla", Titulo = "Bolsonaro condenado bla bla bla", Imagem = "https://lh3.googleusercontent.com/proxy/CQNLceqsuuD3aRhxn9oKRBIONhWUeqMoLlEXRsDqomabPiUSZ2GNBXq47HWXHsRDP46XckwmxGGbkQHndfpeOwErluLwbDJp9AGO_yWkkdX9_1HK8NUSytWmGfoLTyldz5hWcx4"}
-                }
-            };
+        [HttpGet("query/{query}")]
+        public async Task<BingNewsResponse> GetAsync(string query)
+        {
+            var client = new BingSearchClient();
+            var response = await client.Request(query);
 
-            return result;
+            return response;
         }
     }
 }
